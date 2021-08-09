ffc488bacf914cd1bffe8aa6aebb2a84e916892d
Rman22 <51508552+Rman22@users.noreply.github.com>
2021-08-09 21:19:55 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'customGreen':'#39b54a;',
+        'customBlack':'#121212;'
+      }
+    },
+
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
